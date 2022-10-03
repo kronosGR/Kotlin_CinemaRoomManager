865801742779ed724fc16383748d1f4dbc1f4e4a
KronosGR <geo.elgeo@gmail.com>
2022-10-03 15:32:07 +0200

    stage 1/5

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,5 +1,32 @@
 package cinema
 
+
+fun putSpace(arr: Array<Array<String>>) {
+    for (y in arr.indices) {
+        for (x in arr[y].indices) {
+            arr[y][x] = "S"
+        }
+    }
+}
+
+fun printCinema(arr: Array<Array<String>>) {
+    println("Cinema:")
+    println("  1 2 3 4 5 6 7 8")
+    for (y in arr.indices) {
+        print("${y + 1} ")
+        for (x in arr[y].indices) {
+            print(arr[y][x])
+            if (x < arr[y].size - 1) {
+                print(" ")
+            }
+        }
+        println()
+    }
+}
+
 fun main() {
     // write your code here
+    val seats: Array<Array<String>> = Array(7) { row -> Array(8) { col -> "S" } }
+    putSpace(seats)
+    printCinema(seats)
 }
