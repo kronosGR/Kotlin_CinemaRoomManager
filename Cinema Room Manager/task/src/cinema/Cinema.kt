caa448b6e01953a375a6db9d84c4061b2af25852
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-10-07 15:58:22 +0200

    Update Cinema.kt
    stage 5/5 completed

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,10 +1,11 @@
 package cinema
 
-
-var income = 0
 var rows: Int = 0
 var seatsPerRow: Int = 0
 var seats: Array<Array<String>> = arrayOf()
+var totalSeats = 0
+var currentIncome = 0
+
 fun askTheaterSize() {
     println("Enter the number of rows:")
     rows = readln().toInt()
@@ -12,6 +13,7 @@ fun askTheaterSize() {
     seatsPerRow = readln().toInt()
 
     seats = Array(rows) { row -> Array(seatsPerRow) { col -> "S" } }
+    totalSeats = rows * seatsPerRow
 }
 
 
@@ -54,8 +56,38 @@ fun bookTicket(row: Int, col: Int): Int {
         }
     }
 
-    seats[row - 1][col - 1] = "B";
-    return price;
+    if (row < 1 || row > seats.size || col < 1 || col > seats[0].size) {
+        return -1;
+    } else if (seats[row - 1][col - 1] == "S") {
+        seats[row - 1][col - 1] = "B";
+        currentIncome += price;
+        return price;
+    }
+    return 0;
+}
+
+fun getTotalBooked(): Int {
+    var totalBooked = 0;
+
+    for (y in seats.indices) {
+        for (x in seats.indices) {
+            if (seats[y][x].equals("B")) {
+                totalBooked++;
+            }
+        }
+    }
+    return totalBooked;
+}
+
+fun calcTotalIncome(): Int {
+    if (rows * seatsPerRow <= 60) {
+        return totalSeats * 10;
+    } else {
+        var half = rows / 2;
+        var totalPrice = rows / 2 * seatsPerRow * 10;
+        totalPrice += ((rows / 2) + 1) * seatsPerRow * 8;
+        return totalPrice;
+    }
 }
 
 fun showMenu() {
@@ -64,50 +96,60 @@ fun showMenu() {
     while (true) {
         println("1. Show the seats")
         println("2. Buy a ticket")
+        println("3. Statistics")
         println("0. Exit")
         val option = readln().toInt()
 
         when (option) {
             1 -> printCinema()
             2 -> buyTicket()
+            3 -> showStatistics()
             0 -> break
         }
     }
 }
 
 fun buyTicket() {
-    println()
-    println("Enter a row number:")
-    val row = readln().toInt()
-    println("Enter a seat number in that row:")
-    val col = readln().toInt()
+    while (true) {
+        println()
+        println("Enter a row number:")
+        val row = readln().toInt()
+        println("Enter a seat number in that row:")
+        val col = readln().toInt()
 
-    println("Ticket price: $" + bookTicket(row, col))
-    println();
+        var price = bookTicket(row, col)
+        if (price != 0 && price != -1) {
+            println("Ticket price: $" + price)
+            break;
+        } else if (price == -1) {
+            println("Wrong input!")
+        } else {
+            println("That ticket has already been purchased!")
+        }
+
+        System.out.println();
+    }
+}
+
+fun showStatistics() {
+    var totalBooked = getTotalBooked()
+
+    println("Number of purchased tickets: " + totalBooked)
+    if (totalBooked == 0) {
+        println("Percentage: 0.00%")
+    } else {
+        var per = (totalBooked.toFloat() / totalSeats * 100)
+        var msg = String.format("Percentage: %3.2f%%", per)
+        println(msg)
+    }
+    println("Current income: $" + currentIncome)
+    println("Total income: $" + calcTotalIncome())
+    println()
+
 }
 
 fun main() {
-    // write your code here
-//    putSpace(seats)
-//    printCinema(seats)
-
     askTheaterSize()
     showMenu()
 
-//    if (rows * seatsPerRow <= 60) {
-//        income = rows * seatsPerRow * 10;
-//    } else {
-//        var half = rows / 2;
-//        if (rows % 2 == 0) {
-//            income = half * seatsPerRow * 10;
-//            income += half * seatsPerRow * 8;
-//        } else {
-//            income = half * seatsPerRow * 10;
-//            income += (half + 1) * seatsPerRow * 8;
-//
-//        }
-//    }
-//
-//    println("Total income:")
-//    println("$$income")
 }
